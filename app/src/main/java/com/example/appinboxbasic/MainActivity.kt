4a3270a19ccc373f4797377c1af1f858fe5b7f53
Sunny <sl2883@cornell.edu>
2021-07-29 16:56:12 -0700

    adding button

--- app/src/main/java/com/example/appinboxbasic/MainActivity.kt
+++ app/src/main/java/com/example/appinboxbasic/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.appinboxbasic
-import android.app.NotificationManager        //added by CleverTap Assistant
-import com.clevertap.android.sdk.CleverTapAPI //added by CleverTap Assistant
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.clevertap.android.sdk.CleverTapAPI
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,14 @@ class MainActivity : AppCompatActivity() {
         CleverTapAPI.setDebugLevel(3)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-		CleverTapAPI.createNotificationChannel(getApplicationContext(),"generic","mychannel","lDescription",NotificationManager.IMPORTANCE_MAX,true)        //added by CleverTap Assistant
+		CleverTapAPI.createNotificationChannel(getApplicationContext(), "generic", "mychannel", "lDescription", NotificationManager.IMPORTANCE_MAX, true)        //added by CleverTap Assistant
+
+        val button: Button = findViewById(R.id.button_id)
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                // Code here executes on main thread after user presses button
+                
+            }
+        })
     }
 }
