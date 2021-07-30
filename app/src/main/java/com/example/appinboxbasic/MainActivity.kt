e8ac079a6d1b8b6d103ac1ef5c43eb4f794a66aa
Sunny <sl2883@cornell.edu>
2021-07-29 17:11:47 -0700

    app inbox setup

--- app/src/main/java/com/example/appinboxbasic/MainActivity.kt
+++ app/src/main/java/com/example/appinboxbasic/MainActivity.kt
@@ -5,10 +5,11 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.clevertap.android.sdk.CTInboxListener
 import com.clevertap.android.sdk.CleverTapAPI
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CTInboxListener {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         var clevertapDefaultInstance: CleverTapAPI? = null
@@ -17,13 +18,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 		CleverTapAPI.createNotificationChannel(getApplicationContext(), "generic", "mychannel", "lDescription", NotificationManager.IMPORTANCE_MAX, true)        //added by CleverTap Assistant
+        clevertapDefaultInstance?.apply {
 
+            ctNotificationInboxListener = this@MainActivity
+
+            //Initialize the inbox and wait for callbacks on overridden methods
+            initializeInbox()
+        }
+
+    }
+
+    override fun inboxDidInitialize() {
         val button: Button = findViewById(R.id.button_id)
-        button.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                // Code here executes on main thread after user presses button
-                
+        button.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v:View) {
+                var clevertapDefaultInstance: CleverTapAPI? = null
+                clevertapDefaultInstance = CleverTapAPI.getDefaultInstance(applicationContext)   //Initializing the CleverTap SDK
+
+                clevertapDefaultInstance?.showAppInbox()//Opens Activity with default style config
             }
         })
+    }
+
+    override fun inboxMessagesDidUpdate() {
+
     }
 }
